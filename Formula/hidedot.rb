689e9d53d611ec425c60a8f68407078e22e7f9ea
Youri T. K. K. Mattar <youri@youhide.com.br>
2025-02-10 16:50:22 -0300

    Brew formula update for hideDot version v0.0.3

--- Formula/hidedot.rb
+++ Formula/hidedot.rb
@@ -11,18 +11,18 @@ class Hidedot < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/youhide/hideDot/releases/download/v0.0.3/hideDot_Darwin_x86_64.tar.gz"
-      sha256 "3e746bb78d0ec5b4752be420d130a08ad7e532471f012d1a416d4853542d51ea"
+      sha256 "abc8b80aa941e3d5acbc40a18054a1aa982faee526fd9f8634838b060a706b68"
 
       def install
-        bin.install "hidedot"
+        bin.install "hideDot" => "hidedot"
       end
     end
     if Hardware::CPU.arm?
       url "https://github.com/youhide/hideDot/releases/download/v0.0.3/hideDot_Darwin_arm64.tar.gz"
-      sha256 "5493197dfece2a2c8d9161fcbdb6659f89ffd7422155f25303a7dc0d422a9b18"
+      sha256 "e38bbbd7333339a891914799bcd3a1e508020895c132d89752960c1d2dcf2cac"
 
       def install
-        bin.install "hidedot"
+        bin.install "hideDot" => "hidedot"
       end
     end
   end
@@ -31,20 +31,20 @@ class Hidedot < Formula
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
         url "https://github.com/youhide/hideDot/releases/download/v0.0.3/hideDot_Linux_x86_64.tar.gz"
-        sha256 "be1c9c52b0c038d01b1cbc7b04cce33c29ae5c74bfd3d51edd61e6d0135e5a40"
+        sha256 "e29de7536cde2bc73d8adfd1062307b7f28439f8e81e972ceb5942e3965086a3"
 
         def install
-          bin.install "hidedot"
+          bin.install "hideDot" => "hidedot"
         end
       end
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
         url "https://github.com/youhide/hideDot/releases/download/v0.0.3/hideDot_Linux_arm64.tar.gz"
-        sha256 "8faa563524e218d6c2b00348ffd4376c3ae049fddbd7afa55b6154b28fdb9b43"
+        sha256 "a3fc31b0e0d348cd8746e0e5e9359c2ddf49825cff6ad8abe316c417a3e6024d"
 
         def install
-          bin.install "hidedot"
+          bin.install "hideDot" => "hidedot"
         end
       end
     end
