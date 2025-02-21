35d200ad1ff57e7d539efc1b2ef2915a6734d5a0
Youri T. K. K. Mattar <youri@youhide.com.br>
2025-02-20 23:45:05 -0300

    Brew formula update for hideDot version v0.0.4

--- Formula/hidedot.rb
+++ Formula/hidedot.rb
@@ -5,21 +5,21 @@
 class Hidedot < Formula
   desc "A dotfiles manager written in Go"
   homepage "https://github.com/youhide/hideDot"
-  version "0.0.3"
+  version "0.0.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/youhide/hideDot/releases/download/v0.0.3/hideDot_Darwin_x86_64.tar.gz"
-      sha256 "abc8b80aa941e3d5acbc40a18054a1aa982faee526fd9f8634838b060a706b68"
+      url "https://github.com/youhide/hideDot/releases/download/v0.0.4/hideDot_Darwin_x86_64.tar.gz"
+      sha256 "020e74620ab663929bea63c350f3d85e31154143b5d709078d58c27b7b65659a"
 
       def install
         bin.install "hideDot" => "hidedot"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/youhide/hideDot/releases/download/v0.0.3/hideDot_Darwin_arm64.tar.gz"
-      sha256 "e38bbbd7333339a891914799bcd3a1e508020895c132d89752960c1d2dcf2cac"
+      url "https://github.com/youhide/hideDot/releases/download/v0.0.4/hideDot_Darwin_arm64.tar.gz"
+      sha256 "985131349502c130d0d451271ead7c338862b1171c1771f3d7eb37346f59510f"
 
       def install
         bin.install "hideDot" => "hidedot"
@@ -30,8 +30,8 @@ class Hidedot < Formula
   on_linux do
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/youhide/hideDot/releases/download/v0.0.3/hideDot_Linux_x86_64.tar.gz"
-        sha256 "e29de7536cde2bc73d8adfd1062307b7f28439f8e81e972ceb5942e3965086a3"
+        url "https://github.com/youhide/hideDot/releases/download/v0.0.4/hideDot_Linux_x86_64.tar.gz"
+        sha256 "48615bc18d843b719b552b82270c8331b45e1b6d2b539fb363316a1a2c1338f0"
 
         def install
           bin.install "hideDot" => "hidedot"
@@ -40,8 +40,8 @@ class Hidedot < Formula
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/youhide/hideDot/releases/download/v0.0.3/hideDot_Linux_arm64.tar.gz"
-        sha256 "a3fc31b0e0d348cd8746e0e5e9359c2ddf49825cff6ad8abe316c417a3e6024d"
+        url "https://github.com/youhide/hideDot/releases/download/v0.0.4/hideDot_Linux_arm64.tar.gz"
+        sha256 "d92eee08080f351057e14e4d260b8b65680383f5f1f2915fc2bf83416139920a"
 
         def install
           bin.install "hideDot" => "hidedot"
