0ab59e201665f32da8d5ebb5e1f78a6724c30cde
Youri T. K. K. Mattar <youri@youhide.com.br>
2026-01-12 22:32:59 -0300

    Brew formula update for hideDot version v0.0.5

--- Formula/hidedot.rb
+++ Formula/hidedot.rb
@@ -5,21 +5,21 @@
 class Hidedot < Formula
   desc "A dotfiles manager written in Go"
   homepage "https://github.com/youhide/hideDot"
-  version "0.0.4"
+  version "0.0.5"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/youhide/hideDot/releases/download/v0.0.4/hideDot_Darwin_x86_64.tar.gz"
-      sha256 "020e74620ab663929bea63c350f3d85e31154143b5d709078d58c27b7b65659a"
+      url "https://github.com/youhide/hideDot/releases/download/v0.0.5/hideDot_Darwin_x86_64.tar.gz"
+      sha256 "c7150383092a81d70aff65e62ed944dc64c049ef542e8d46cc810cf18e0acf37"
 
       def install
         bin.install "hideDot" => "hidedot"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/youhide/hideDot/releases/download/v0.0.4/hideDot_Darwin_arm64.tar.gz"
-      sha256 "985131349502c130d0d451271ead7c338862b1171c1771f3d7eb37346f59510f"
+      url "https://github.com/youhide/hideDot/releases/download/v0.0.5/hideDot_Darwin_arm64.tar.gz"
+      sha256 "4b992902709e8d7ccd8d24655f458e0f910aa20716b17e8aa767b9297d46e2ba"
 
       def install
         bin.install "hideDot" => "hidedot"
@@ -28,24 +28,18 @@ class Hidedot < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      if Hardware::CPU.is_64_bit?
-        url "https://github.com/youhide/hideDot/releases/download/v0.0.4/hideDot_Linux_x86_64.tar.gz"
-        sha256 "48615bc18d843b719b552b82270c8331b45e1b6d2b539fb363316a1a2c1338f0"
-
-        def install
-          bin.install "hideDot" => "hidedot"
-        end
+    if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+      url "https://github.com/youhide/hideDot/releases/download/v0.0.5/hideDot_Linux_x86_64.tar.gz"
+      sha256 "2325f34d5568538d7741a2a8971eb4194cb28a681135b15274df880c8873eace"
+      def install
+        bin.install "hideDot" => "hidedot"
       end
     end
-    if Hardware::CPU.arm?
-      if Hardware::CPU.is_64_bit?
-        url "https://github.com/youhide/hideDot/releases/download/v0.0.4/hideDot_Linux_arm64.tar.gz"
-        sha256 "d92eee08080f351057e14e4d260b8b65680383f5f1f2915fc2bf83416139920a"
-
-        def install
-          bin.install "hideDot" => "hidedot"
-        end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/youhide/hideDot/releases/download/v0.0.5/hideDot_Linux_arm64.tar.gz"
+      sha256 "d6c4e4b7a91aa925ffb99657cafa8385e88b14abf0fc021253a19959752e7e57"
+      def install
+        bin.install "hideDot" => "hidedot"
       end
     end
   end
